7e37223f2bdf9322bced0f1b89057c9417a8f818
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-11-22 12:17:29 -0500

    readme

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Liya 
+ * Created on: Nov 2023
+ * This program makes wheels move forward unless there's an object within 10 cm.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Asleep)
