0b14c635a1aca34c709bcf826ad6779e9290df8c
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-11-29 12:34:01 -0500

    ts vers

--- main.ts
+++ main.ts
@@ -5,9 +5,37 @@
  * This program makes wheels move forward unless there's an object within 10 cm.
 */
 
-// variables 
+// variables
 let distanceToObject: number = 0
 
 // setup
+basic.clearScreen()
 basic.showIcon(IconNames.Asleep)
-basic.pause(1000)
+
+while (true) {
+  if (input.buttonIsPressed(Button.A) === true) {
+    while (true) {
+      // find distance from sonar
+        basic.clearScreen()
+        distanceToObject = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+
+        // turn if distance < 10
+      if (distanceToObject < 10) {
+        robotbit.StpCarMove(-10, 48)
+        basic.pause(500)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        basic.pause(500)
+        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+        basic.pause(500)
+        robotbit.StpCarMove(10, 48)
+      } else {
+        // move forward
+        robotbit.StpCarMove(10, 48)
+      }
+    }
+  }
+}
